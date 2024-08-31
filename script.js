04fa408afb47115c9f1f8c0093359885702ae3ad
YahirJr03 <161895444+YahirJr03@users.noreply.github.com>
2024-08-31 08:13:27 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+// Script para funciones adicionales (placeholder para futuras expansiones)
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('JavaScript cargado y listo!');
+});
